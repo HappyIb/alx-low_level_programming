ad9df9debc7beba6af9a95a0808a5d811f66f511
HappyIb <ibjack23@gmail.com>
2023-06-18 07:35:30 -0700

    second bettyprintf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main - A program that prints a line using the printf function
+ * main - A program that prints a line using the printf function
  * Return: 0
  */
 int main(void)
