d6f3c532ee64f24c5b1611f589cb9922be42416d
HappyIb <ibjack23@gmail.com>
2023-06-19 12:53:50 -0700

    quote text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints exactly a given sentence followed by a new line, to the standard error
+ *
+ * Return: 1 (success)
+ */
+ int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
