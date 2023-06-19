73afd69266b5ad44a8ee306ed16aa4d7a88f855f
HappyIb <ibjack23@gmail.com>
2023-06-19 10:19:03 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio>
+/**
+ * main - A program that codes for the printing of the size of various computer types
+ * Return 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int d;
+	float f;
+printf("size of a chart: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
