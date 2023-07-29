87f8e200e86ce065cc4eb3556f7a02d66334e623
HappyIb <ibjack23@gmail.com>
2023-07-28 21:13:52 -0700

    A function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,50 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
-
+int len(const char *str);
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
- *
  * Return: the address of the new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *new;
- unsigned int len = 0;
+	list_t *new_node;
 
- while (str[len])
- len++;
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return ('\0');
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+	{
+		new_node->next = *head;
+		new_node->str = strdup(str);
+		new_node->len = len(str);
+		*head = new_node;
+	}
+	return (*head);
+}
 
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
+/**
+ * len - get lenght of string
+ * @str: string to get lenght
+ *
+ * Return: lenght of the string
+ */
 
- return (*head);
+int len(const char *str)
+{
+	int l;
+
+	if (str == NULL)
+		return (0);
+	l = 0;
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
 }
